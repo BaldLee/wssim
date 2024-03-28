f10bdb603221641244a07acc47fb40cc842ee52b
BaldLee <baldlee@outlook.com>
2024-03-28 11:07:21 +0800

    Update CMakeLists.txt and inline member func
    Split CMakeLists.txt into directories. Now simple member functions are inline, which is defined in header files. It could improve the performance.

--- include/utils/card.hh
+++ include/utils/card.hh
@@ -13,14 +13,39 @@ class card {
     card(int _level, int _type, int _trigger)
         : __level(_level), __type(_type), __trigger(_trigger) {}
 
-    void print() const;
-    std::string to_str() const;
-    int level() const;
-    int type() const;
-    int trigger() const;
-    void set_level(const int _level);
-    void set_type(const int _type);
-    void set_trigger(const int _trigger);
+    inline std::string to_str() const {
+        std::stringstream ss;
+        std::string type_str;
+        switch (__type) {
+            case CHAR:
+                type_str = "CHAR";
+                break;
+            case CLIMAX:
+                type_str = "CLIMAX";
+                break;
+            case EVENT:
+                type_str = "EVENT";
+                break;
+            default:
+                type_str = "NOTYPE";
+        }
+        ss << "{lv" << __level << "," << type_str << "," << __trigger << "} ";
+        return ss.str();
+    }
+
+    inline void print() const { std::cout << to_str(); }
+
+    inline int level() const { return __level; }
+
+    inline int type() const { return __type; }
+
+    inline int trigger() const { return __trigger; }
+
+    inline void set_level(const int _level) { __level = _level; }
+
+    inline void set_type(const int _type) { __type = _type; }
+
+    inline void set_trigger(const int _trigger) { __trigger = _trigger; }
 
    private:
     int __level;
--- include/utils/deck.hh
+++ include/utils/deck.hh
@@ -14,17 +14,41 @@ class deck {
    public:
     deck() {}
 
-    card& peek_top();
-    card& peek_bottom();
-    void push_top(const card& c);
-    void push_bottom(const card& c);
-    card pop_top();
-    card pop_bottom();
-    std::size_t size() const;
-    void print() const;
-    void shuffle();
-    void clear();
-    card& at(int index);
+    inline std::size_t size() const { return __card_deque.size(); }
+
+    inline card& peek_top() { return __card_deque.front(); }
+
+    inline card& peek_bottom() { return __card_deque.back(); }
+
+    inline void push_top(const card& c) { __card_deque.push_front(c); }
+
+    inline void push_bottom(const card& c) { __card_deque.push_back(c); }
+
+    inline card pop_top() {
+        auto top = __card_deque.front();
+        __card_deque.pop_front();
+        return top;
+    }
+
+    inline card pop_bottom() {
+        auto bottom = __card_deque.front();
+        __card_deque.pop_front();
+        return bottom;
+    }
+
+    inline void print() const {
+        std::for_each(__card_deque.begin(), __card_deque.end(),
+                      [](auto x) { x.print(); });
+        std::cout << std::endl;
+    }
+
+    inline void shuffle() {
+        std::shuffle(__card_deque.begin(), __card_deque.end(), rng);
+    }
+
+    inline void clear() { __card_deque.clear(); }
+
+    inline card& at(int index) { return __card_deque.at(index); }
 
    private:
     // <TOP> [card, card, ..., card] <BOTTOM>
